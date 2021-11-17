1b5e3582c4a8f93124ebb8eedd94e68d55df05ca
rajib miah <ahmedrajib942@gmail.com>
2021-11-17 15:45:59 +0600

    update

---
+++ advance_py/index.py
@@ -0,0 +1,51 @@
+
+
+
+
+
+# decorators
+
+# def mydecorator(function):
+    
+#     def wrapper( *args , **kwargs):
+#         print('I am decorating your function! ')
+#         return function( *args , **kwargs)
+#     return wrapper    
+
+
+# decorator practical exam #2 - timing
+# import time
+# def timed(function):
+
+#     def wrapper(*args , **kwargs):
+#         before = time.time()
+#         return_value = function(*args , **kwargs)
+#         after = time.time()
+#         fname = function.__name__
+#         print(f"{fname} took {after-before} seconds to execute!")
+#         return return_value
+#     return wrapper
+# @timed
+# def myfunction(x):
+#     result = 1
+#     for i in range(1 , x):
+#         result *= i
+#     return result
+
+
+# myfunction(90000)
+
+# practical example #1 - logging
+
+# def logged(function):
+
+#     def wrapper( *args , **kwargs):
+#         pass
+
+
+
+# @mydecorator
+# def hello_worrd(person):
+#     print(f"hello {person}!")
+
+# hello_worrd("rajib")
---
+++ advance_py/tempCodeRunnerFile.py
@@ -0,0 +1,20 @@
+import time
+def timed(function):
+
+    def wrapper(*args , **kwargs):
+        before = time.time()
+        return_value = function(*args , **kwargs)
+        after = time.time()
+        fname = function.__name__
+        print(f"{fname} took {after-before} seconds to execute!")
+        return return_value
+
+@timed
+def myfunction(x):
+    result = 1
+    for i in range(1 , x):
+        result *= i
+    return result
+
+
+myfunction(500000)
