ee0bf86c96ca19fbb588055898c75906b85ec825
rajib miah <ahmedrajib942@gmail.com>
2021-11-26 23:40:19 +0600

    solving sum of nested list using recursion

--- advance_py/recursion.py
+++ advance_py/recursion.py
@@ -1,15 +1,15 @@
-# def factorial(n):
-#     print(n) # 5 4 3 2 1
-#     if n == 1:
-#        return n
 
-#     return n * factorial(n-1)
+#problem sum of all list  ===> x = [[1,2] ,[[3] 4, [5 , 6]] ,[7,[8]]]
 
-def sum_into_sequence(current_number , accumulated_sum ):
-    if current_number  == 11:
-        return accumulated_sum
-    return sum_into_sequence(current_number + 1 , accumulated_sum + current_number)    
+def addition(array):
+    total = 0
+    for x in array:
+        if type(x) != list:
+            total += x
+        else:
+            total += addition(x)
+    return total
+   
 
 if __name__ == "__main__":
-    # print(factorial(5))  # output 120
-    print(sum_into_sequence(1 , 0)) 
+    print(addition([[1,2] ,[[3], 4, [5 , 6]] ,[7,[8]]]))
