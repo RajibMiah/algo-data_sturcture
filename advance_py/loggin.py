5775562d821b944b4cf2a62bf836e15afd1b4d81
rajib miah <ahmedrajib942@gmail.com>
2021-12-01 23:01:47 +0600

    logger in python

---
+++ advance_py/loggin.py
@@ -0,0 +1,15 @@
+import logging
+from logging import root
+
+def test():
+    print('-'*30)
+    level = logging.getLevelName(logging.getLogger().getEffectiveLevel())
+    print(f"Log Level:{level}")
+    logging.debug('debug message here')
+    logging.info('inf message here')
+    logging.warning('warring message here')
+    logging.error('error message here')
+    logging.critical('critical message here')
+    print('-'*30)
+
+test()
