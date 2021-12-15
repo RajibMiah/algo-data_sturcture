ec5c7540ca7f78534d25fc58f0cc210c5de4527e
rajibmiah <ahmedrajib942@gmail.com>
2021-12-15 23:59:57 +0600

    funtool

--- advance_py/lamda_fun.py
+++ advance_py/lamda_fun.py
@@ -1,21 +1,36 @@
+from functools import reduce
 
 # simple lamda expression
 
-# x = lambda x : x + 10
+x = lambda x : x + 10
 
-# x = lambda y , z : y + z
+x = lambda y , z : y + z
 
-# x = lambda *args : sum(args)
+x = lambda *args : sum(args)
 
-# x = lambda **kwargs: sum(kwargs.values())
+x = lambda **kwargs: sum(kwargs.values())
 
-# print(x( a = 10 , b = 2 , c = 3 , d = 5 , e = 6))
+
+
+# using functools 
 
 num = [ 1 , 2 , 3 , 4 , 5]
 
-add = map( lambda x: x + 10 , num )
+add = list(map( lambda x: x + 10 , num ))
 
-print(list(add))
+filter_num = list(filter(lambda x: x % 2  , num)) # returnd all odd number
 
+cumulative_sum = reduce(lambda x , y : x +  y , range(1 , 10))
+
+def count(x):
+    y = x + 10
+    return (lambda x : x + 10) (y)
+
+print("add number" ,add )
+print('filter numbers ', filter_num)
+print('cumulative sum' , cumulative_sum)
+
+
+print(count(10))    
 
 
